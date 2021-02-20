c96e9db8ddd55a3602aeeb2c78879c06688da6f8
kathedore <katekakhniauri@gmail.com>
2021-02-20 20:41:05 +0400

    second commit (won't work with gson deserialization)

--- src/main/java/mainclass.java
+++ src/main/java/mainclass.java
@@ -20,37 +20,16 @@ public class mainclass extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-/*
+
         List<MyRes> myres = new ArrayList<>();
         myres = s2j.getMyRes();
         Gson gson = new Gson();
         String response = gson.toJson(myres);
-*/
-        String json = "{\n" +
-                "   \"command\":\"DailyRapprt\",\n" +
-                "   \"response\":\"Ok/WrongDateSpan/WrongScope/NoData\",\n" +
-                "   \"ReportBody\":[\n" +
-                "      {\n" +
-                "         \"param1\":\"value1\",\n" +
-                "         \"param2\":\"value2\",\n" +
-                "         \"param3\":\"value3\",\n" +
-                "         \"param8\":\"value8\"\n" +
-                "      },\n" +
-                "      {\n" +
-                "         \"param1\":\"value1\",\n" +
-                "         \"param2\":\"value2\",\n" +
-                "         \"param3\":\"value3\",\n" +
-                "         \"param8\":\"value8\"\n" +
-                "      }\n" +
-                "   ]\n" +
-                "}\n";
-        JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
 
         PrintWriter pw = res.getWriter();
         res.setContentType("application/json");
         res.setCharacterEncoding("UTF-8");
-        //pw.write(response);
-        pw.write(String.valueOf(jsonObject));
+        pw.write(response);
         pw.close();
 
     }
